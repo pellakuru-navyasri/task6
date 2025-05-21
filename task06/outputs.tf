3eb17a92eb60beef1f48e7cb365eb6263a757ec0
Navyasri <pellakuru_navyasri@epam.com>
2025-05-21 16:39:38 +0530

    only 2 params

--- task06/outputs.tf
+++ task06/outputs.tf
@@ -2,12 +2,6 @@ output "sql_server_fqdn" {
   value       = module.sql.sql_server_fqdn
   description = "Fully qualified domain name of the SQL Server"
 }
-
-output "sql_connection_string" {
-  value       = module.sql.sql_connection_string
-  description = "ADO.NET SQL connection string"
-  sensitive   = true
-}
 output "app_hostname" {
   description = "Hostname of the Web App from webapp module"
   value       = module.webapp.app_hostname
