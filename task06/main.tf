199777c1cbfbaecfc4081f0ccb15d752e9bd6f7f
Navyasri <pellakuru_navyasri@epam.com>
2025-05-23 13:15:08 +0530

    chnaged

--- task06/main.tf
+++ task06/main.tf
@@ -18,13 +18,12 @@ module "sql" {
   sql_fwr_name              = var.sql_fwr_name
   sql_admin_username        = var.sql_admin_username
   location                  = var.location
-  resource_group_name       = local.rg_name
+  resource_group_name       = azurerm_resource_group.rg.name
   allowed_ip_address        = var.allowed_ip_address
   key_vault_id              = data.azurerm_key_vault.existing_kv.id
   sql_admin_secret_name     = var.sql_admin_secret_name
   sql_admin_secret_password = var.sql_admin_secret_password
   tags                      = var.tags
-  depends_on                = [azurerm_resource_group.rg]
 }
 module "webapp" {
   source = "./modules/webapp"
@@ -33,11 +32,11 @@ module "webapp" {
   asp_name              = local.asp_name
   asp_sku               = var.asp_sku
   location              = var.location
-  rg_name               = local.rg_name
+  rg_name               = azurerm_resource_group.rg.name
   sql_connection_string = module.sql.sql_connection_string
   app_dotnet_version    = var.app_dotnet_version
   tags                  = var.tags
-  depends_on            = [azurerm_resource_group.rg]
+  
 }
 
 
